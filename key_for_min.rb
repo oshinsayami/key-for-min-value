183ede118c0156df07514f749cd975c79db0382f
Simran <mdrsimran@gmail.com>
2020-10-26 21:07:02 -0500

    commit tree e659f8d87fd37b52494e14e4bff072782fc4217c

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    if name_hash.empty?
+        return nil
+    end
+    
+    min=nil
+    return_key=""
+name_hash.each do |key, value|
+    if min == nil
+        min=value
+        return_key = key
+    elsif value<min
+        return_key=key
+        min=value
+    end
+    
+    
+end
+return_key
 
 end
